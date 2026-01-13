8ab940f8dd31703079e1620e8b392bbd67fceea9
Thiago Fontes <lasnoches13@gmail.com>
2026-01-13 17:24:38 -0300

    reajuste de view

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'recipes/index.html')
+    return render(request,'recipes/index.html',context={
+        'name':'Thiago Fontes'
+    })
 
 def about(request):
     return HttpResponse('Essa é a página sobre')
