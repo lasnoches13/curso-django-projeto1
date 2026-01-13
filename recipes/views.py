766df13f560b7d953b0b36d8bba9b687523b8c58
Thiago Fontes <lasnoches13@gmail.com>
2026-01-12 21:37:50 -0300

    font-awesome

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request,'recipes/index.html')
 
-def about(request):
-    return HttpResponse('Essa é a página sobre')
