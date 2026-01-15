1ad62b10da3e92ac9f9ffc5b95e43050c77d2026
Thiago Fontes <lasnoches13@gmail.com>
2026-01-14 21:13:48 -0300

    Ajustes do html

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'recipes/index.html',context={
+    return render(request,'recipes/pages/index.html',context={
         'name':'Thiago Fontes'
     })
 
