30287cacf5b5c27469f1f986a22af2607ce14c80
Thiago Fontes <lasnoches13@gmail.com>
2026-01-19 15:27:30 -0300

    templates prontos para o backend

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'base_templates',
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,4 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'recipes/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'recipes/pages/index.html',context={
+    return render(request,'recipes/pages/home.html',context={
         'name':'Thiago Fontes'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
 
