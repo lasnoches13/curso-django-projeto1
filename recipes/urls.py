ac412b4c584c61853e7a61f7ae7a42192ea9391d
Thiago Fontes <lasnoches13@gmail.com>
2026-01-26 20:57:04 -0300

    transformando a receita em link para detalhes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='recipes-home'),# name cria um nome proprio (string) e único para esse path. podendo ser usado em links e botões dentro do html, como em 'header.html' linha 4 
+    path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
 ] 
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request,'recipes/pages/home.html',context={
-        'name':'Thiago Fontes'
+        'recipes': [make_recipe() for _ in range(10)],#envia para home.html linha 07
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
-
+    return render(request, 'recipes/pages/recipe-view.html',context={
+        'recipe':make_recipe(),#faker
+        'is_datail_page':True,# retira botão caso seja pagina de detalhes
+        })
