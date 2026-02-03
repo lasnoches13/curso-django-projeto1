54d2d466f3a0a23297f3d36456ded3d83a88ccd5
Thiago Fontes <lasnoches13@gmail.com>
2026-02-03 12:34:42 -0300

    pagina detalhe mostrando receita escolhida na home (id=id)

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id') #Mosrtra na tela todas as receitas cadastradas no DB por ordem descrescente de id
     return render(request,'recipes/pages/home.html',context={
-        'recipes': [make_recipe() for _ in range(10)],#envia para home.html linha 07
+        'recipes': recipes #Manda para o template home.html a variável instanciada na ln 6
     })
 
 def recipe(request, id):
+    recipee = Recipe.objects.get(id=id) # id do parametro da função será igual o id do Recipe.object. A pagina só mostrará uma unica receita cujo os ids são iguais
     return render(request, 'recipes/pages/recipe-view.html',context={
-        'recipe':make_recipe(),#faker
+        'recipe': recipee,
         'is_datail_page':True,# retira botão caso seja pagina de detalhes
         })
